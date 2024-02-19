a45a58bdabfa78ac26636c972b553a123c378f08
Milad Nekofar <milad@nekofar.com>
2024-02-19 11:42:05 +0330

    feat: migrate to cloudflare pages

--- apps/nouns-camp/src/app/page.js
+++ apps/nouns-camp/src/app/page.js
@@ -1,6 +1,8 @@
 import ClientAppProvider from "./client-app-provider.js";
 import BrowseScreen from "../components/browse-screen.js";
 
+export const runtime = 'edge';
+
 export default function Page() {
   return (
     <ClientAppProvider>
