4bfc84a37ad04b53e34cbdf11eb33a5c6045a59b
olli <1403501+werme@users.noreply.github.com>
2024-04-02 18:30:18 +0200

    Campers -> Voters
    (cherry picked from commit 8343d274391f926634ef01db58e25b79100ece19)

--- apps/nouns-camp/src/app/voters/page.js
+++ apps/nouns-camp/src/app/voters/page.js
@@ -1,7 +1,7 @@
 import ClientAppProvider from "../client-app-provider.js";
 import BrowseAccountsScreen from "../../components/browse-accounts-screen.js";
 
-// export const runtime = "edge";
+export const runtime = "edge";
 
 export default function Page() {
   return (
