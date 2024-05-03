baad6494638cffc704fc58889cef97e2b15aaf80
olli <1403501+werme@users.noreply.github.com>
2024-05-03 09:37:55 +0400

    Add support for casting to candidate urls
    (cherry picked from commit e3fc985cfab515ac53864ccd129e7c5a3a7e3feb)

--- apps/nouns-camp/src/app/api/farcaster-accounts/route.js
+++ apps/nouns-camp/src/app/api/farcaster-accounts/route.js
@@ -1,47 +1,8 @@
 import { kv } from "@vercel/kv";
 import { isAddress } from "viem";
-import { array as arrayUtils } from "@shades/common/utils";
-import { parseNeynarUsers } from "../utils.js";
+import { fetchAccountsWithVerifiedAddress } from "../farcaster-utils.js";
 
 export const runtime = "edge";
-
-const fetchAccountsWithVerifiedAddress = async (
-  address,
-  { chainId = 1 } = {},
-) => {
-  const searchParams = new URLSearchParams({
-    addresses: address,
-    address_types: "verified_addresss",
-  });
-
-  const response = await fetch(
-    `https://api.neynar.com/v2/farcaster/user/bulk-by-address?${searchParams}`,
-    {
-      headers: {
-        accept: "application/json",
-        api_key: process.env.NEYNAR_API_KEY,
-      },
-    },
-  );
-
-  const body = await response.json();
-
-  if (!response.ok) {
-    console.error(body);
-    return null;
-  }
-
-  const neynarUsers = body[address] ?? [];
-
-  if (neynarUsers.length === 0) return 0;
-
-  const sortedUsers = arrayUtils.sortBy(
-    { value: (u) => u.follower_count, order: "desc" },
-    neynarUsers,
-  );
-
-  return parseNeynarUsers({ chainId }, sortedUsers);
-};
 
 // Returns Farcaster accounts matching a verified Ethereum account address
 export async function GET(request) {
