ef9a6eead6380a437b697b02f1af033f7b8b5ccb
Milad Nekofar <milad@nekofar.com>
2024-06-04 14:58:19 +0400

    feat: add farcaster APIs for handling casts, proposals, and accounts
    This commit introduces several new APIs within the 'farcaster' module for handling casts, proposals, and account data. These include the ability to fetch and post proposal and candidate casts, grab verified Ethereum account addresses, retrieve and set farcaster account keys, among others. The changes encompass a new routing mechanism, business logic, and error handling, providing enhanced capabilities for working with farcaster components.

--- apps/nouns-camp/src/app/api/farcaster-accounts/route.js
+++ apps/nouns-camp/src/app/api/farcaster-accounts/route.js
@@ -1,8 +1,6 @@
-// import { kv } from "@vercel/kv";
+import { kv } from "@vercel/kv";
 import { isAddress } from "viem";
-// import { fetchAccountsWithVerifiedAddress } from "../farcaster-utils.js";
-
-export const runtime = "edge";
+import { fetchAccountsWithVerifiedAddress } from "../farcaster-utils.js";
 
 // Returns Farcaster accounts matching a verified Ethereum account address
 export async function GET(request) {
@@ -17,13 +15,13 @@ export async function GET(request) {
       },
     });
 
-  const accounts = [] // await fetchAccountsWithVerifiedAddress(ethAddress);
+  const accounts = await fetchAccountsWithVerifiedAddress(ethAddress);
 
   const accountsWithKeyData = await Promise.all(
     accounts.map(async (account) => {
       // `exists` returns the number of existing keys
-      const hasAccountKey = false
-        // (await kv.exists(`fid:${account.fid}:account-key`)) > 0;
+      const hasAccountKey =
+        (await kv.exists(`fid:${account.fid}:account-key`)) > 0;
       return { ...account, hasAccountKey };
     }),
   );
