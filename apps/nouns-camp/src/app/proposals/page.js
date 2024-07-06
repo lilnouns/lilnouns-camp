1bea4a6cd9f455f327e0cc828a4e247af296d972
Milad Nekofar <milad@nekofar.com>
2024-07-07 00:08:48 +0400

    fix: add runtime constant to candidates and proposals pages
    Added `runtime` constant set as 'edge' in both candidates and proposals pages within the application. This helps configure the runtime environment for these specific pages to enhance their execution behaviour.

--- apps/nouns-camp/src/app/proposals/page.js
+++ apps/nouns-camp/src/app/proposals/page.js
@@ -1,6 +1,8 @@
 import ClientAppProvider from "../client-app-provider.js";
 import BrowseProposalsScreen from "../../components/browse-proposals-screen.js";
 
+export const runtime = "edge";
+
 export default function Page() {
   return (
     <ClientAppProvider>
