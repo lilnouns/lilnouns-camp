ab2fc2e80f14e836d5e6c1b78767a22f43e44e57
Milad Nekofar <milad@nekofar.com>
2024-08-21 20:05:16 +0400

    style(treasury-dialog): comment out unused import
    Comment out import statement for `buildEtherscanLink` as it is currently unused to clean up the codebase.

--- apps/nouns-camp/src/components/treasury-dialog.js
+++ apps/nouns-camp/src/components/treasury-dialog.js
@@ -22,7 +22,7 @@ import FormattedNumber from "./formatted-number.js";
 import ExplorerAddressLink from "./chain-explorer-address-link.js";
 import Link from "@shades/ui-web/link";
 import NextLink from "next/link";
-import { buildEtherscanLink } from "../utils/etherscan.js";
+// import { buildEtherscanLink } from "../utils/etherscan.js";
 
 const ONE_DAY_IN_SECONDS = 24 * 60 * 60;
 
