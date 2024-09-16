187f5b67edfcda89217ac55c91435385c14a271a
Milad Nekofar <milad@nekofar.com>
2024-09-16 23:31:24 +0400

    feat(api): enable farcaster transaction like handling
    remove commented-out code and restore functionality for GET and POST methods handling likes on farcaster transactions.

--- apps/nouns-camp/src/app/api/farcaster-transaction-likes/route.js
+++ apps/nouns-camp/src/app/api/farcaster-transaction-likes/route.js
@@ -1,36 +1,35 @@
-// import { CHAIN_ID } from "@/constants/env";
-// import { parseEpochTimestamp } from "@/utils/farcaster";
-// import { createUri as createTransactionReceiptUri } from "@/utils/erc-2400";
-// import { isLoggedIn, isLoggedInAccountFid } from "@/app/api/auth-utils";
-// import {
-//   fetchNounerLikesByTargetUrl,
-//   submitReactionAdd,
-//   submitReactionRemove,
-// } from "@/app/api/farcaster-utils";
-// import {
-//   getAccountKeyForFid,
-//   deleteAccountKeyForFid,
-// } from "../farcaster-account-key-utils";
+import { CHAIN_ID } from "@/constants/env";
+import { parseEpochTimestamp } from "@/utils/farcaster";
+import { createUri as createTransactionReceiptUri } from "@/utils/erc-2400";
+import { isLoggedIn, isLoggedInAccountFid } from "@/app/api/auth-utils";
+import {
+  fetchNounerLikesByTargetUrl,
+  submitReactionAdd,
+  submitReactionRemove,
+} from "@/app/api/farcaster-utils";
+import {
+  getAccountKeyForFid,
+  deleteAccountKeyForFid,
+} from "../farcaster-account-key-utils";
 
 export const runtime = 'edge';
 
-// eslint-disable-next-line no-unused-vars
 export async function GET(request) {
-  // const { searchParams } = new URL(request.url);
-  // const hash = searchParams.get("hash");
-  //
-  // if (hash == null)
-  //   return Response.json({ error: "hash-required" }, { status: 400 });
-  //
-  // const likes = await fetchNounerLikesByTargetUrl(
-  //   createTransactionReceiptUri(CHAIN_ID, hash),
-  // );
-  //
-  // // const filteredLikes = likes.filter((l) => l.votingPower > 0);
-  // const filteredLikes = likes.filter((l) => l.nounerAddress != null);
+  const { searchParams } = new URL(request.url);
+  const hash = searchParams.get("hash");
+
+  if (hash == null)
+    return Response.json({ error: "hash-required" }, { status: 400 });
+
+  const likes = await fetchNounerLikesByTargetUrl(
+    createTransactionReceiptUri(CHAIN_ID, hash),
+  );
+
+  // const filteredLikes = likes.filter((l) => l.votingPower > 0);
+  const filteredLikes = likes.filter((l) => l.nounerAddress != null);
 
   return Response.json(
-    { likes: [] /*filteredLikes*/ },
+    { likes: filteredLikes },
     {
       status: 200,
       headers: {
@@ -40,55 +39,54 @@ export async function GET(request) {
   );
 }
 
-// eslint-disable-next-line no-unused-vars
 export async function POST(request) {
-  // const { transactionHash, fid, action } = await request.json();
-  //
-  // if (!(await isLoggedIn()))
-  //   return Response.json({ error: "not-logged-in" }, { status: 401 });
-  //
-  // if (!(await isLoggedInAccountFid(fid)))
-  //   return Response.json({ error: "address-not-verified" }, { status: 401 });
-  //
-  // const privateAccountKey = await getAccountKeyForFid(fid);
-  //
-  // if (privateAccountKey == null)
-  //   return Response.json({ error: "no-account-key" }, { status: 401 });
-  //
-  // if (transactionHash == null)
-  //   return Response.json(
-  //     { error: "transaction-hash-required" },
-  //     { status: 400 },
-  //   );
-  //
-  // if (action == null)
-  //   return Response.json({ error: "action-required" }, { status: 400 });
-  //
-  // try {
-  //   const submit =
-  //     action === "remove" ? submitReactionRemove : submitReactionAdd;
-  //
-  //   const message = await submit(
-  //     { fid, privateAccountKey },
-  //     {
-  //       type: 1,
-  //       targetUrl: createTransactionReceiptUri(CHAIN_ID, transactionHash),
-  //     },
-  //   );
-  //   return Response.json(
-  //     {
-  //       hash: message.hash,
-  //       fid: message.data.fid,
-  //       timestamp: parseEpochTimestamp(message.data.timestamp).toISOString(),
-  //     },
-  //     { status: 201 },
-  //   );
-  // } catch (e) {
-  //   // Delete revoked key
-  //   if (e.message === "invalid-account-key") {
-  //     await deleteAccountKeyForFid(fid);
-  //     return Response.json({ error: "invalid-account-key" }, { status: 401 });
-  //   }
+  const { transactionHash, fid, action } = await request.json();
+
+  if (!(await isLoggedIn()))
+    return Response.json({ error: "not-logged-in" }, { status: 401 });
+
+  if (!(await isLoggedInAccountFid(fid)))
+    return Response.json({ error: "address-not-verified" }, { status: 401 });
+
+  const privateAccountKey = await getAccountKeyForFid(fid);
+
+  if (privateAccountKey == null)
+    return Response.json({ error: "no-account-key" }, { status: 401 });
+
+  if (transactionHash == null)
+    return Response.json(
+      { error: "transaction-hash-required" },
+      { status: 400 },
+    );
+
+  if (action == null)
+    return Response.json({ error: "action-required" }, { status: 400 });
+
+  try {
+    const submit =
+      action === "remove" ? submitReactionRemove : submitReactionAdd;
+
+    const message = await submit(
+      { fid, privateAccountKey },
+      {
+        type: 1,
+        targetUrl: createTransactionReceiptUri(CHAIN_ID, transactionHash),
+      },
+    );
+    return Response.json(
+      {
+        hash: message.hash,
+        fid: message.data.fid,
+        timestamp: parseEpochTimestamp(message.data.timestamp).toISOString(),
+      },
+      { status: 201 },
+    );
+  } catch (e) {
+    // Delete revoked key
+    if (e.message === "invalid-account-key") {
+      await deleteAccountKeyForFid(fid);
+      return Response.json({ error: "invalid-account-key" }, { status: 401 });
+    }
     return Response.json({ error: "submit-failed" }, { status: 500 });
-  // }
+  }
 }
