f13030fd5e6a455687de754d94981aef56865b57
olli <1403501+werme@users.noreply.github.com>
2024-09-24 13:01:17 +0400

    feat(camp): enable auction and noun representation events in the main feed
    This also changes the filter menu to a multi-select variant. Very MVP, but I
    must not scope creep further. 🧘
    
    (cherry picked from commit 8357c3d1ab8e73e3d2a2a09c2a33a0ea1157c448)

--- apps/nouns-camp/src/store.js
+++ apps/nouns-camp/src/store.js
@@ -17,12 +17,12 @@ import {
   isActiveState as isActiveProposalState,
 } from "./utils/proposals.js";
 import {
-  // buildAuctionFeed,
+  buildAuctionFeed,
   buildAccountFeed,
   buildProposalFeed,
   buildCandidateFeed,
   buildPropdateFeedItem,
-  // buildNounsTokenRepresentationFeed,
+  buildNounsTokenRepresentationFeed,
 } from "./store-selectors/feeds.js";
 import {
   extractSlugFromId as extractSlugFromCandidateId,
@@ -2493,7 +2493,7 @@ export const useAccountFeedItems = (accountAddress, { filter }) => {
   );
 };
 
-export const useMainFeedItems = (filter, { enabled = true }) => {
+export const useMainFeedItems = (categories, { enabled = true }) => {
   const eagerLatestBlockNumber = useBlockNumber({
     watch: true,
     cacheTime: 10_000,
@@ -2518,11 +2518,6 @@ export const useMainFeedItems = (filter, { enabled = true }) => {
           casts,
         );
 
-        // const buildAuctionItems = () => buildAuctionFeed(s);
-
-        // const buildNounsTokenRepresentationItems = () =>
-        //   buildNounsTokenRepresentationFeed(s);
-
         const buildProposalItems = () =>
           Object.keys(s.proposalsById).flatMap((proposalId) =>
             buildProposalFeed(s, proposalId, {
@@ -2545,29 +2540,40 @@ export const useMainFeedItems = (filter, { enabled = true }) => {
             propdates.map(buildPropdateFeedItem),
           );
 
-        const buildFeedItems = () => {
-          switch (filter) {
-            case "proposals":
-              return [...buildProposalItems(), ...buildPropdateItems()];
-            case "candidates":
-              return buildCandidateItems();
-            case "propdates":
-              return buildPropdateItems();
-            default:
-              return [
+        const feedItems =
+          categories.length === 0
+            ? [
+                ...buildAuctionFeed(s),
+                ...buildNounsTokenRepresentationFeed(s),
                 ...buildProposalItems(),
                 ...buildCandidateItems(),
                 ...buildPropdateItems(),
-              ];
-          }
-        };
+              ]
+            : categories.flatMap((category) => {
+                switch (category) {
+                  case "auction-excluding-bids":
+                    return buildAuctionFeed(s, { excludeBids: true });
+                  case "auction-bids":
+                    return buildAuctionFeed(s, { bidsOnly: true });
+                  case "noun-representation":
+                    return buildNounsTokenRepresentationFeed(s);
+                  case "proposals":
+                    return buildProposalItems();
+                  case "candidates":
+                    return buildCandidateItems();
+                  case "propdates":
+                    return buildPropdateItems();
+                  default:
+                    console.error(`Unrecognized category: "${category}"`);
+                }
+              });
 
         return arrayUtils.sortBy(
           { value: (i) => i.blockNumber ?? 0, order: "desc" },
-          buildFeedItems(),
+          feedItems,
         );
       },
-      [enabled, filter, casts, latestBlockNumber],
+      [enabled, categories, casts, latestBlockNumber],
     ),
   );
 };
