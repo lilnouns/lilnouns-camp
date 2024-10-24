6accaa0ea4cd425fb562befce9ae79307f738ecb
olli <1403501+werme@users.noreply.github.com>
2024-10-24 12:20:51 +0400

    feat(app): set runtime to `edge` for noun and auction pages
    Transition to edge runtime for improved performance and lower latency.

--- apps/nouns-camp/src/app/auction/page.js
+++ apps/nouns-camp/src/app/auction/page.js
@@ -2,6 +2,8 @@ import ClientAppProvider from "@/app/client-app-provider";
 import { build as buildMetadata } from "@/utils/metadata";
 import NounScreen from "@/components/noun-screen";
 
+export const runtime = "edge";
+
 export const metadata = buildMetadata({
   title: "Auction",
   canonicalPathname: "/auction",
