9108f4ae43e0ae6d0f4f3353994e31d3b77cc096
Milad Nekofar <milad@nekofar.com>
2025-01-29 01:08:09 +0300

    feat(common): add `useUNSName` hook export
    enable fetching UNS (Universal Name Service) names. This addition enhances name resolution capabilities.

--- packages/common/src/ethereum-react/index.js
+++ packages/common/src/ethereum-react/index.js
@@ -1,2 +1,3 @@
 export { default as useAccountDisplayName } from "./hooks/account-display-name.js";
 export { useNNSName } from "./hooks/nns-name.js";
+export { useUNSName } from "./hooks/uns-name.js";
