1812a4fd0c9c450a14ba4bc7f45bfed4aba9b6e9
Milad Nekofar <milad@nekofar.com>
2025-02-02 19:07:57 +0300

    fix(app): add edge runtime for topics page
    Enable `edge` runtime for improved performance and scalability.

--- apps/nouns-camp/src/app/topics/page.js
+++ apps/nouns-camp/src/app/topics/page.js
@@ -2,6 +2,8 @@ import ClientAppProvider from "../client-app-provider.js";
 import { build as buildMetadata } from "../../utils/metadata.js";
 import BrowseCandidatesScreen from "../../components/browse-candidates-screen.js";
 
+export const runtime = "edge";
+
 export const metadata = buildMetadata({
   title: "Topics",
   canonicalPathname: "/topics",
