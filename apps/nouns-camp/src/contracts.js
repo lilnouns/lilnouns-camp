6a60d2227afbe894780c48ffed44a3bc70462dcf
Milad Nekofar <milad@nekofar.com>
2025-04-12 22:04:03 +0300

    fix(contracts): update Lil Nouns `data` contract address
    update ensures accurate contract interactions.

--- apps/nouns-camp/src/contracts.js
+++ apps/nouns-camp/src/contracts.js
@@ -66,7 +66,7 @@ const addressByIdentifierByChainId = {
     // Lil Nouns contracts
     dao: "0xa7c37f79ff5e6f932147fc69724b6ed432ca6aa7",
     executor: "0xe54f098b1880c536e0083720922b8a365fb403dc",
-    data: "0x2019c935757866b0F5c72A34D5B0F1035ff06e55",
+    data: "0x15427d3d1550f333a235c3a8dbc98c19e6adef31",
     token: "0x6e48e79f718776cf412a87e047722dbfda5b465d",
     descriptor: "0x852f20f0140a4b5aa29c70bf39c9a85edc2b454e",
     "auction-house": "0x0d8c4d18765ab8808ab6cee4d7a760e8b93ab20c",
