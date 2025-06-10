0637b4f4b9e0d15bebab41d83ed13bd4e02e45c1
Milad Nekofar <milad@nekofar.com>
2025-06-10 18:56:17 +0300

    refactor(nouns-camp): comment out unused rewrite rule
    disable the unused `rewrites` mapping to improve clarity and reduce potential confusion. maintains a cleaner configuration while aligning with current usage.
    
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- apps/nouns-camp/next.config.mjs
+++ apps/nouns-camp/next.config.mjs
@@ -103,7 +103,7 @@ const nextConfig = {
   },
   rewrites() {
     return [
-      { source: "/topics/:path*", destination: "/candidates/:path*" },
+      // { source: "/topics/:path*", destination: "/candidates/:path*" },
       { source: "/sw.js", destination: "/service-worker.js" },
       {
         source: "/subgraphs/nouns",
