1edf4fc16e1c7813f8cb95f06d94b98f0f21badf
Milad Nekofar <milad@nekofar.com>
2025-07-12 04:49:25 +0300

    fix(nouns-camp): update query to use dynamic variables
    pass `id` through `variables` object for improved clarity. ensures consistency with API best practices and enhances maintainability.
    
    Signed-off-by: Milad Nekofar <milad@nekofar.com>
    (cherry picked from commit 36dd1025766acbfdd1bb98eba56204f91340f37f)

--- apps/nouns-camp/src/app/api/simulate/candidate/[id]/route.js
+++ apps/nouns-camp/src/app/api/simulate/candidate/[id]/route.js
@@ -6,8 +6,8 @@ export const runtime = 'edge';
 const fetchCandidate = async (id) => {
   const data = await subgraphFetch({
     query: `
-      query {
-        proposalCandidate(id: ${JSON.stringify(id)}) {
+      query($id: ID!) {
+        proposalCandidate(id: $id) {
           id
           slug
           proposer
@@ -23,6 +23,7 @@ const fetchCandidate = async (id) => {
           }
         }
       }`,
+    variables: { id },
   });
 
   if (data?.proposalCandidate == null) return null;
