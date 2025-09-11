e19ef8526de3ead786387050122d2392a455efe5
Milad Nekofar <milad@nekofar.com>
2025-09-11 03:56:19 +0300

    refactor(components): remove unused draft navigation logic
    Simplify `ProposalOfTopicEditorScreen` by removing outdated handling of draft navigation and search parameters.
    
    This improves code maintainability by eliminating unused logic and dependencies.
    
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- apps/nouns-camp/src/components/proposal-or-topic-editor-screen.jsx
+++ apps/nouns-camp/src/components/proposal-or-topic-editor-screen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NextLink from "next/link";
 import { formatEther, parseUnits } from "viem";
 import { css, useTheme } from "@emotion/react";
-import { useFetch, useLatestCallback } from "@shades/common/react";
+import { useFetch } from "@shades/common/react";
 import {
   // invariant,
   message as messageUtils,
@@ -26,7 +26,7 @@ import {
   useActiveProposalId,
 } from "@/hooks/dao-contract";
 import { useActions, useAccountProposalCandidates } from "@/store";
-import { useNavigate, useSearchParams } from "@/hooks/navigation";
+import { useNavigate } from "@/hooks/navigation";
 import {
   useCreateProposalCandidate,
   useProposalCandidateCreateCost,
@@ -591,10 +591,8 @@ const SubmitDialog = ({
 
 export default function ProposalOfTopicEditorScreen({ draftId }) {
   const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
 
   const [draft] = useDraft(draftId);
-  const { items: drafts, createItem: createDraft } = useDrafts();
 
   const [hasPendingNavigationTransition, startNavigationTransition] =
     React.useTransition();
@@ -607,57 +605,6 @@ export default function ProposalOfTopicEditorScreen({ draftId }) {
       navigate("/", { replace: true });
     }
   }, [draftId, draft, navigate, hasPendingNavigationTransition]);
-
-  const targetType = searchParams.get("topic") != null ? "topic" : "proposal";
-
-  const getFirstEmptyDraft = useLatestCallback(() =>
-    drafts.find((draft) => {
-      const hasEmptyName = draft.name.trim() === "";
-      const hasEmptyBody =
-        draft.body === "" ||
-        (draft.body.length === 1 && isRichTextEditorNodeEmpty(draft.body[0]));
-
-      if (targetType === "topic") {
-        const isTopicDraft = draft.actions == null;
-        return isTopicDraft && hasEmptyName && hasEmptyBody;
-      }
-
-      const isProposalDraft = draft.actions != null;
-
-      return (
-        isProposalDraft &&
-        hasEmptyName &&
-        hasEmptyBody &&
-        draft.actions.length === 0
-      );
-    }),
-  );
-
-  React.useEffect(() => {
-    if (draftId != null || createDraft == null) return;
-
-    const preExistingEmptyDraft = getFirstEmptyDraft();
-
-    const newSearchParams = new URLSearchParams(searchParams);
-    newSearchParams.delete("topic");
-
-    if (preExistingEmptyDraft != null) {
-      navigate(`/new/${preExistingEmptyDraft.id}?${newSearchParams}`, {
-        replace: true,
-      });
-      return;
-    }
-
-    const draft = createDraft({ actions: targetType === "topic" ? null : [] });
-    navigate(`/new/${draft.id}?${newSearchParams}`, { replace: true });
-  }, [
-    draftId,
-    targetType,
-    createDraft,
-    getFirstEmptyDraft,
-    navigate,
-    searchParams,
-  ]);
 
   if (draft == null) return null; // Spinner
 
