3970987a55f5575e4d941fd62259997482e1c40f
Milad Nekofar <milad@nekofar.com>
2025-09-22 21:01:35 +0300

    fix(nouns-camp): consolidate draft logic in editor screen
    Moved `draft` handling and related logic entirely to `ProposalOrTopicEditorScreen` to simplify and streamline code. This eliminates redundancy and improves maintainability.
    
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- apps/nouns-camp/src/app/new/[[...segments]]/page.js
+++ apps/nouns-camp/src/app/new/[[...segments]]/page.js
@@ -1,13 +1,10 @@
 "use client";
-import React, { use, useEffect } from "react";
+import React, { use } from "react";
 
 import { useWallet } from "@/hooks/wallet";
 import ClientAppProvider from "@/app/client-app-provider";
 import ProposalOrTopicEditorScreen from "@/components/proposal-or-topic-editor-screen";
 import ConnectWalletScreen from "@/components/connect-wallet-screen";
-import { useCollection as useDrafts } from "@/hooks/drafts";
-import { useDialog } from "@/hooks/global-dialogs";
-import { useNavigate, useSearchParams } from "@/hooks/navigation";
 
 export default function Page(props) {
   const params = use(props.params);
@@ -15,42 +12,11 @@ export default function Page(props) {
   return (
     <ClientAppProvider>
       <RequireConnectedAccount>
-        <PageContent draftId={draftId} />
+        <ProposalOrTopicEditorScreen draftId={draftId} />
       </RequireConnectedAccount>
     </ClientAppProvider>
   );
 }
-
-const PageContent = ({ draftId }) => {
-  const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
-  const { createItem: createDraft } = useDrafts();
-
-  const { open: openTemplateDialog, isOpen: isTemplateDialogOpen } =
-    useDialog("proposal-templates");
-
-  const hasTopicParam = searchParams.get("topic") != null;
-
-  useEffect(() => {
-    if (draftId == null && hasTopicParam) {
-      const draft = createDraft({ actions: null });
-      navigate(`/new/${draft.id}`, { replace: true });
-      return;
-    }
-
-    if (draftId == null) openTemplateDialog();
-  }, [draftId, hasTopicParam, createDraft, navigate, openTemplateDialog]);
-
-  useEffect(() => {
-    if (draftId == null && !isTemplateDialogOpen && !hasTopicParam) {
-      navigate("/", { replace: true });
-    }
-  }, [draftId, hasTopicParam, isTemplateDialogOpen, navigate]);
-
-  if (draftId == null) return null;
-
-  return <ProposalOrTopicEditorScreen draftId={draftId} />;
-};
 
 const RequireConnectedAccount = ({ children }) => {
   const { address: connectedAccountAddress } = useWallet();
