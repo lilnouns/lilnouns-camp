127147a0ca4edc67fd260b2493df23c92f450158
Milad Nekofar <milad@nekofar.com>
2025-09-24 23:41:09 +0300

    refactor(routes): move /topics→/candidates rewrite to middleware

--- apps/nouns-camp/src/middleware.js
+++ apps/nouns-camp/src/middleware.js
@@ -2,12 +2,22 @@ import { NextResponse } from "next/server";
 
 const BUILD_ID = process.env.BUILD_ID;
 
-// eslint-disable-next-line no-unused-vars
 export function middleware(request) {
-  const res = NextResponse.next();
-  res.headers.set("x-camp-build-id", BUILD_ID);
+  const { nextUrl } = request;
+  const { pathname } = nextUrl;
 
-  return res;
+  // Rewrite /topics/:path* to /candidates/:path*
+  let response;
+  if (pathname === "/topics" || pathname.startsWith("/topics/")) {
+    const url = nextUrl.clone();
+    url.pathname = pathname.replace(/^\/topics(\/|$)/, "/candidates$1");
+    response = NextResponse.rewrite(url);
+  } else {
+    response = NextResponse.next();
+  }
+
+  response.headers.set("x-camp-build-id", BUILD_ID);
+  return response;
 }
 
 export const config = {
